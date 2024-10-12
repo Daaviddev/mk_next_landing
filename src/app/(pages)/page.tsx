5f93fb25875f60c1581552a88fb9b375a4991c7e
Daaviddev <daviddev.exozen@gmail.com>
2024-10-12 16:57:05 +0200

    Build Fix

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,7 +2,6 @@ import About from '@/app/components/About';
 import Contact from '@/app/components/Contact';
 import Featured from '@/app/components/Featured';
 import Hero from '@/app/components/Hero';
-import LogoScroll from '@/app/components/LogoScroll';
 import Testemonial from '@/app/components/Testemonial';
 
 export default function Home() {
--- src/app/components/LogoScroll.tsx
+++ src/app/components/LogoScroll.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import Audi from '@/app/assets/Logos/audi.svg';
